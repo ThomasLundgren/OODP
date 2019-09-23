d4d29229d5ac9d31aca2dd9e397cce51378e7329
Thomas Lundgren <thomasLundgren@live.com>
2019-09-23 10:42:43 +0200

    Lab 2 done

--- oodp-lab-2/test/se/hig/thlu/clock/WeekAlarmClockTest.java
+++ oodp-lab-2/test/se/hig/thlu/clock/WeekAlarmClockTest.java
@@ -18,27 +18,73 @@ class WeekAlarmClockTest {
 	private AlarmType alarm;
 	private AlarmActionType alarmAction;
 	private AlarmClockType alarmClock;
-	private TimeType time;
+	private TimeType alarmTime;
+	private TimeType clockTime;
 
 	@BeforeEach
 	void setUp() throws Exception {
-		time = new Time(0, 0, 0, 0);
+		alarmTime = new Time(0, 0, 0, 5);
+		clockTime = new Time(0, 0, 0, 0);
 		alarmAction = new PrintAlarmAction();
-		alarm = new Alarm(time, alarmAction);
-		alarmClock = new WeekAlarmClock(time, alarm);
+		alarm = new Alarm(alarmTime, alarmAction);
+		alarmClock = new WeekAlarmClock(clockTime, alarm);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
-		time = null;
+		alarmTime = null;
 		alarmAction = null;
 		alarm = null;
 		alarmClock = null;
 	}
-
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void timeAndAlarmConstructor_passNullTime_throwsNpe() {
+		assertThrows(NullPointerException.class, () -> {
+			new WeekAlarmClock(null, alarm);
+		});
+	}
+	
+	@Test
+	void timeAndAlarmConstructor_passNullAlarm_throwsNpe() {
+		assertThrows(NullPointerException.class, () -> {
+			new WeekAlarmClock(clockTime, null);
+		});
+	}
+	
+	@Test
+	void alarmOnlyConstructor_passNullAlarm_throwsNpe() {
+		assertThrows(NullPointerException.class, () -> {
+			new WeekAlarmClock(null);
+		});
+	}
+	
+	@Test
+	void getAlarms_afterAddAlarm_alarmIsAdded() {
+		TimeType newTime = new Time(1, 1, 1, 1);
+		AlarmActionType newAlarmAction = new PrintAlarmAction();
+		AlarmType newAlarm = new Alarm(newTime, newAlarmAction);
+		alarmClock.addAlarm(newAlarm);
+		assertTrue(alarmClock.getAlarms().contains(newAlarm));
+	}
+	
+	@Test
+	void getAlarms_afterRemoveAlarm_alarmIsRemoved() {
+		TimeType newTime = new Time(1, 1, 1, 1);
+		AlarmActionType newAlarmAction = new PrintAlarmAction();
+		AlarmType newAlarm = new Alarm(newTime, newAlarmAction);
+		alarmClock.addAlarm(newAlarm);
+		
+		assertTrue(alarmClock.getAlarms().contains(newAlarm));
+		alarmClock.removeAlarm(newAlarm);
+		assertFalse(alarmClock.getAlarms().contains(newAlarm));
+	}
+	
+	@Test
+	void tickTock_oneTime_increasesTimeByOne() {
+		assertTrue(clockTime.getSecond() == 0);
+		alarmClock.tickTock();
+		assertTrue(clockTime.getSecond() == 1);
 	}
 
 }
